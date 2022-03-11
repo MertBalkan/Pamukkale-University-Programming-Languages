a69393e353c2dd0828f385e5be3f0a70b9cd430a
TheMOON <mert__balkan@hotmail.com>
2022-03-11 18:49:28 +0300

    lambda example added

---
+++ Scheme Programming Language/lambda-example.rkt
@@ -0,0 +1,4 @@
+#lang racket
+;lambda ozel formu: belirtilen bicimsel parametrelerle bir fonksiyon
+;uygulandiginda degerlendirilecek bir kod govdesi olusturur
+((lambda (radius) (* 3.14(* radius radius))) 10) 
