44a13ae01f78851b39432e000f2ccb090a01b185
TheMOON <mert__balkan@hotmail.com>
2022-04-08 14:22:22 +0300

    en-cok-tekrarlayan-kelime 100/100

--- Week5~Ruby/en-cok-tekrarlayan-kelime.rb
+++ Week5~Ruby/en-cok-tekrarlayan-kelime.rb
@@ -1,4 +1,3 @@
-
 # Kodunuzu bu dosya içine yazınız
 =begin
 Kodunuzun düzgün çalışması için
@@ -6,3 +5,24 @@ input metodunun içindeki metni boş bırakın
 gets.chomp
 yeterli...
 =end
+girdi = gets.chomp
+
+girdi.gsub! ".","" # ! ile degistiriliyor ve girdiye ataniyor
+girdi.gsub! ",",""
+girdi.gsub! "'",""
+girdi.gsub! "-",""
+girdi.downcase!
+
+h = Hash.new
+
+girdi.split.each do |kelime|
+    if h.include? kelime
+        h[kelime] += 1
+    else
+        h[kelime] = 1
+    end
+end
+
+
+a = h.sort_by{ |k, v| -v} #key ve value, -v => azalan
+puts a[0..4].to_h
