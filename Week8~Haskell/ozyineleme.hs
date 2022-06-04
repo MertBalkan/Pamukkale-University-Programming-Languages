07d5066af46f0c55a9c1ca6588f6e93837e9413f
Mert Balkan <mert__balkan@hotmail.com>
2022-06-04 19:50:13 +0300

    100/100

--- Week8~Haskell/ozyineleme.hs
+++ Week8~Haskell/ozyineleme.hs
@@ -2,20 +2,29 @@ module Ogrenci where --Bu satırı değiştirmeyin/silmeyin
 
 --uzunluk fonksiyonunu yazın(listenin uzunluğunu verir)
 uzunluk::[a]->Int
-uzunluk _ = error "uzunluk henuz tanimlanmadi"
+uzunluk [] = 0
+uzunluk(x:xs) = 1+uzunluk xs
 
 --ciftFaktoryel fonksiyonunu yazın
 ciftFaktoryel::Integer->Integer
-ciftFaktoryel _ = error "ciftFaktoryel henuz tanimlanmadi"
+ciftFaktoryel 1 = 1
+ciftFaktoryel 2 = 2 
+ciftFaktoryel n + n*ciftFaktoryel(n-2)
 
 --panindrom fonksiyonunu yazın(bir listenin/metnin palindrom olup olmadığını verir)
 palindrom::(Eq a)=>[a]->Bool
-palindrom _ = error "palindrom henuz tanimlanmadi"
+palindrom [] = True
+palindrom [_] = True
+palindrom(x:xs) = x==last xs && palindrom (init xs)
 
 --indistekiEleman fonksiyonunu yazın(tuple olarak verilen liste ve indise göre bir eleman verir)
 indistekiEleman::([a], Int)->a
-indistekiEleman _ = error "indistekiEleman henuz tanimlanmadi"
+indistekiEleman ((x:_), 0) = x
+indistekiEleman((x:xs), i) = indistekiEleman(xs, (i - 1))
 
 --compress fonksiyonunu yazın
 compress::(Eq a)=>[a]->[a]
-compress _ = error "compress henuz tanimlanmadi"
+compress [x] = [x]
+compress (x:y:xs)
+	| x == y = compress(y:xs)
+	| otherwise = x:compress (y:xs)
